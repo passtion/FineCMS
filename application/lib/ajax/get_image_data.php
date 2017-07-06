601a9721d9ad0e8904a826e2a49386befa3f436a
passtion <sun@whitecell-club.org>
2017-07-06 22:12:59 +0800

    Update get_image_data.php
    Fix ssrf by sun@yuesec.com
    Description:
    1、Filter the file suffix：'jpg', 'jpeg', 'gif', 'png', 'swf', 'bmp'
    2、Modify the controllable variable:
    $_SERVER['HTTP_HOST']  --> /config/config.php DOMAIN_NAME

--- application/lib/ajax/get_image_data.php
+++ application/lib/ajax/get_image_data.php
@@ -1,11 +1,19 @@
 <?php
 
+require '../../../config/config.php';
 header('Access-Control-Allow-Origin: *');
 
+function getFileType($filename)
+{
+	return substr($filename, strrpos($filename, '.') + 1);
+}
 if (isset($_GET['id']) && isset($_GET['folder']))
 {
-	$image_data = file_get_contents($_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/'.$_GET['folder'].$_GET['id']);
-	echo base64_encode($image_data);
+	if (in_array(getFileType($_GET['id']), array('jpg', 'jpeg', 'gif', 'png', 'swf', 'bmp')))
+	{
+		$image_data = file_get_contents($_SERVER['REQUEST_SCHEME'].'://'.DOMAIN_NAME.'/'.$_GET['folder'].$_GET['id']);
+		echo base64_encode($image_data);
+	}
 }
 
 ?>
